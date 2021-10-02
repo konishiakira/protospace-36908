093580a13f84c3210930df48884aa636b68af944
konishiakira <kurokan014sdgo22@gmail.com>
2021-10-02 20:40:13 +0900

    謎の修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,7 +13,6 @@ class CommentsController < ApplicationController
 
   end
 
-
   private
   def comment_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id,prototype_id: params[:prototype_id])
